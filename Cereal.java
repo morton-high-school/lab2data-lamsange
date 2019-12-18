af67e19c3476bae23640b71743118408db9fb622
Sam Lange <samuel.lange@mcusd709.org>
2019-12-18 09:39:42 -0600

    Cereal average ratings

--- Cereal.java
+++ Cereal.java
@@ -32,31 +32,50 @@ public class Cereal{
   }
 
   public static void main(String[] args) throws IOException{
-//    ArrayList<Cereal> allCereal = new ArrayList();
+    ArrayList<Cereal> allCereal = new ArrayList();
     Scanner s = new Scanner(new File("Cereal.csv"));
-    s.useDelimiter(",|\n");
+    s.useDelimiter(",");
     for(int i = 0; i < 15; i++){
-      System.out.println(s.next());
+      s.next();
     }
-    System.out.println("\n\n");
     while(s.hasNext()){
-      String n = s.nextLine();
-      System.out.println(n);
-      System.out.println("\n\n");
+      String n = s.next();
       for(int i = 0; i<10; i++){
-        System.out.println(s.next());
+        s.next();
       }
-      System.out.println("\n\n");
       int sh = s.nextInt();
-      System.out.println(sh);
-      System.out.println("\n\n");
-      System.out.println(s.next());
-      System.out.println(s.nextDouble());
-      System.out.println("\n\n");
-      //double rate = s.nextDouble();
-      System.out.println(s.nextDouble());
-//      Cereal c1 = new Cereal(n, sh, rate);
-//      allCereal.add(c1);
+      s.next();
+      s.next();
+      String ha = s.next();
+      double rate = Double.parseDouble(ha);
+      Cereal c1 = new Cereal(n, sh, rate);
+      allCereal.add(c1);
     }
+    s.close();
+
+    double sh1 = 0;
+    double sh2 = 0;
+    double sh3 = 0;
+    int sh1n = 0;
+    int sh2n = 0;
+    int sh3n = 0;
+    for(Cereal c : allCereal){
+      if(c.getShelf() == 1){
+        sh1 += c.getRating();
+        sh1n += 1;
+      } else if(c.getShelf() == 2){
+        sh2 += c.getRating();
+        sh2n += 1;
+      }else if(c.getShelf() == 3){
+        sh3 += c.getRating();
+        sh3n += 1;
+      }
+    }
+    sh1 = sh1/sh1n;
+    sh2 = sh2/sh2n;
+    sh3 = sh3/sh3n;
+    System.out.println("The average rating of shelf 1: " + sh1);
+    System.out.println("The average rating of shelf 2: " + sh2);
+    System.out.println("The average rating of shelf 3: " + sh3);
   }
 }
